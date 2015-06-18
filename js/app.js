dd8a64d5c19377f012985f7ba12208fb174dc376
Kevin Lanzon <kevinlanzon@gmail.com>
2015-06-18 16:34:46 +0100

    planning out traversing the list items

--- js/app.js
+++ js/app.js
@@ -57,3 +57,17 @@ var taskIncomplete = function() {
 //Set the click handler to the addTask function
 addButton.onclick = addTask;
 
+//Cycle over the incompleteTasksHolder ul list items
+  //for each list item
+    //select it's children
+    //bind editTask to edit button
+    //bind the deleteTask to the delete button
+    //bind taskCompleted to the checkbox
+
+//Cycle over the completedTasksHolder ul list items
+  //for each list item
+    //select it's children
+    //bind editTask to edit button
+    //bind the deleteTask to the delete button
+    //bind taskIncomplete to the checkbox
+
