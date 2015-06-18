b1a5ef238a05df172f00aed3b74a25e23ca7ce58
Kevin Lanzon <kevinlanzon@gmail.com>
2015-06-18 15:00:23 +0100

    adds comments for sketching out interaction

---
+++ js/app.js
@@ -0,0 +1,41 @@
+//Problem: User interaction doesn't provide desired results.
+//Solution: Add interactivity so the user can manage daily tasks.
+
+//new-task
+//first button
+//incomplete-tasks
+//completed-tasks
+//Add a new task
+
+//Edit an existing task
+  //When the button is pressed
+ //Create a new list item with the test from #new-task
+    //input (checkbox)
+    //label
+    //input (text)
+    //button.edit
+    //button.delete
+    //Each element, needs to be modified and appended
+
+//Edit an existing task
+  //When the dit button is pressed
+    //if the class of the parent is .editMode
+      //switch from editMode
+      //label text become the inputs value
+    //else
+      //switch to editMode
+      //input value = becomes the labels text
+
+    //Toggle .editMode
+
+//Delete existing task
+  //when the delete button is pressed
+    //Remove the parent list from the ul
+
+//Mark a task as complete
+  //when the check bov is checked
+    //we want to append the list item from the #completed-tasks
+
+//Mark a task as incomplete
+  //when the checkbox is unchecked
+    //we weant to append the to #incomplete-tasks
