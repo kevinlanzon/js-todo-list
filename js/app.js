abd0ab255e02e1d0b2b2ad3a983fa70ab610e809
Kevin Lanzon <kevinlanzon@gmail.com>
2015-06-18 15:03:08 +0100

    adds function names

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@
 //completed-tasks
 //Add a new task
 
-//Edit an existing task
+//Add a new task
+var addTask = function() {
   //When the button is pressed
  //Create a new list item with the test from #new-task
     //input (checkbox)
@@ -16,8 +17,10 @@
     //button.edit
     //button.delete
     //Each element, needs to be modified and appended
+};
 
 //Edit an existing task
+var editTask = function() {
   //When the dit button is pressed
     //if the class of the parent is .editMode
       //switch from editMode
@@ -27,15 +30,24 @@
       //input value = becomes the labels text
 
     //Toggle .editMode
+};
 
 //Delete existing task
+var deleteTask = function() {
   //when the delete button is pressed
     //Remove the parent list from the ul
+};
+
 
 //Mark a task as complete
+var taskCompleted = function() {
   //when the check bov is checked
     //we want to append the list item from the #completed-tasks
+};
+
 
 //Mark a task as incomplete
+var taskIncomplete = function() {
   //when the checkbox is unchecked
-    //we weant to append the to #incomplete-tasks
+    //we want to append the to #incomplete-tasks
+};
