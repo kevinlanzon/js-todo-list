5f54d96b28ef13032cc0f01b2d4357ecf7e04aea
Kevin Lanzon <kevinlanzon@gmail.com>
2015-06-18 16:50:10 +0100

    adds for loop that iterates through incompleteTasksHolder

--- js/app.js
+++ js/app.js
@@ -65,8 +65,10 @@ var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
 addButton.onclick = addTask;
 
 //Cycle over the incompleteTasksHolder ul list items
+for(var i = 0; i < incompleteTasksHolder.children.length; i++) {
   //for each list item
     //bind events to list items children (taskCompleted)
+};
 
 //Cycle over the completedTasksHolder ul list items
   //for each list item
